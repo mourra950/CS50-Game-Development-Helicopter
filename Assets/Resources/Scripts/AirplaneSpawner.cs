de2b2c395352372ad1cd6320a082b180bb16f39a
Omar Mohamed <mourra24@gmail.com>
2021-12-26 17:27:33 +0200

    added the finished project files
    game finished

---
+++ Assets/Resources/Scripts/AirplaneSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirplaneSpawner : MonoBehaviour
+{
+    public GameObject[] prefabs;
+
+
+    void Start()
+    {
+
+        // trigger asynchronous randomized infinite spawning of airplanes
+        StartCoroutine(SpawnAirplanes());
+    }
+    IEnumerator SpawnAirplanes()
+    {
+        while (true)
+        {
+
+            // instantiate a random airplane past the right egde of the screen, facing left
+            Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(7, 10), 11),
+                Quaternion.Euler(-90f, -90f, 0f));
+
+            // pause this coroutine for 3-10 seconds and then repeat loop
+            yield return new WaitForSeconds(Random.Range(3, 10));
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/SkyscraperSpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyscraperSpawner : MonoBehaviour
+{
+
+    public GameObject[] prefabs;
+
+    public static float speed = 10f;
+    public float S;
+    void Start()
+    {
+
+
+        StartCoroutine(SpawnSkyscrapers());
+    }
+
+
+    IEnumerator SpawnSkyscrapers()
+    {
+        while (true)
+        {
+            S = speed;
+            Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(-20, -12), 11),
+                Quaternion.Euler(-90f, 0f, 0f));
+
+            // randomly increase the speed by 1
+            if (Random.Range(1, 4) == 1)
+            {
+                speed += 1f;
+            }
+
+            // wait between 1-5 seconds for a new skyscraper to spawn
+            yield return new WaitForSeconds(Random.Range(1, 5));
+        }
+    }
+}
